c980c72ad9ad13bff00b6004a3a7416aa77adc9b
Myeonghak Lee <mhmh779@naver.com>
2023-03-17 15:26:25 +0900

    usage-API

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import lambda_handler
-# from .views import AWSViewSet
+from .views import AWSViewSet
 
-# router = DefaultRouter()
-# router.register('aws', AWSViewSet)
+router = DefaultRouter()
+router.register('aws', AWSViewSet)
 
 urlpatterns = [
-    path('aws/usage/<int>/<int>/', lambda_handler())
+    path('', include(router.urls))
 ]
