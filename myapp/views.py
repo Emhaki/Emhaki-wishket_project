bdd80270b74306229c41ebc8ba978373c0143ea3
Myeonghak Lee <mhmh779@naver.com>
2023-03-20 11:05:44 +0900

    Usage-API

--- myapp/views.py
+++ myapp/views.py
@@ -9,65 +9,40 @@ import urllib.request
 import requests
 from decimal import Decimal
 
-# @api_view(['POST'])
-# def usage(request):
-#     # usage_data = request.data
-#     serializer = UsageDateSerializer(data=request.data)
-
-#     return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-
 class AWSViewSet(viewsets.ViewSet):
     
+    @action(detail=False, methods=['get'])
     def usage(self, request, year, month):
 
         # 예외 처리 조건 2
         # 입력해야 할 year, month 값이 없는 경우 
         if not year or not month:
-            
-            response = {
-                "statusCode": 404,
-                "body": HttpResponse(json.dumps({"message": "입력해야 할 year, month 값이 없습니다."}))
-            }
 
-            return response
+            return Response({'error': "입력해야 할 year, month 값이 없습니다."}, status=status.HTTP_404_NOT_FOUND)
         
         # 데이터가 저장된 zip파일 저장
         url = 'http://dtgqz5l2d6wuw.cloudfront.net/coding_test_1.csv.zip'
-        try:
-            with urllib.request.urlopen(url) as data:
-                # 예외 처리 조건 3
-                # 데이터 URL의 압축 파일이 10MB를 넘는 경우
-                if data.getheader('Content-Length') and int(data.getheader('Content-Length')) > 10*1024*1024:
-                    
-                    response = {
-                        "statusCode": 400,
-                        "body": HttpResponse(json.dumps({"message": '데이터 URL의 압축 파일이 10MB를 넘습니다.'}))
-                    }
-
-                    return response
-
-                # zip파일을 csv로 변환
-                with zipfile.ZipFile(io.BytesIO(data.read())) as zip_file:
-                    csv_filename = zip_file.namelist()[0]
-                    with zip_file.open(csv_filename) as csv_file:
-                        csv_reader = csv.reader(io.TextIOWrapper(csv_file, encoding='utf-8'))
-                        rows = []
-                        for row in csv_reader:
-                            rows.append(row)
 
         # 예외 처리 조건 1
         # 데이터 URL에 파일이 없는 경우
-        except urllib.error.HTTPError as e:
-            if e.code == 404:
-              
-              response = {
-                        "statusCode": 404,
-                        "body": HttpResponse(json.dumps({"message": '데이터 URL에 파일이 없습니다.'}))
-                    }
-                
-            else:
-                raise e
+        if not urllib.request.urlopen(url):
+            return Response({'error': '데이터 URL에 파일이 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
+
+        with urllib.request.urlopen(url) as data:
+            # 예외 처리 조건 3
+            # 데이터 URL의 압축 파일이 10MB를 넘는 경우
+            if data.getheader('Content-Length') and int(data.getheader('Content-Length')) > 10*1024*1024:
+
+                return Response({'error': '데이터 URL의 압축 파일이 10MB를 넘습니다.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            # zip파일을 csv로 변환
+            with zipfile.ZipFile(io.BytesIO(data.read())) as zip_file:
+                csv_filename = zip_file.namelist()[0]
+                with zip_file.open(csv_filename) as csv_file:
+                    csv_reader = csv.reader(io.TextIOWrapper(csv_file, encoding='utf-8'))
+                    rows = []
+                    for row in csv_reader:
+                        rows.append(row)
         
         # 주어진 데이터 엑셀의 TimeInterval 값에서 해당 year-month로 시작하는 데이터 필터링
         filtered_rows = []
@@ -82,15 +57,12 @@ class AWSViewSet(viewsets.ViewSet):
         writer.writerows(filtered_rows)
         csv_content = csv_buffer.getvalue()
 
-        # 필터링 한 값 return
-        return {
-            'statusCode': 200,
-            'headers': {
-                'Content-Type': 'text/csv',
-                'Content-Disposition': f'attachment; filename=usage.csv'
-            },
-            'body': csv_content
+        result = {
+                'ContentType': 'text/csv',
+                'Filename': f'attachment; filename=usage.csv',
+                'body': csv_content
         }
+        return Response(result, status=status.HTTP_200_OK)
     
     @action(detail=False, methods=['post'])
     def bill(self, request):
